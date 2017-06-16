e79641f597eac23c17ebe1837fe630fbde60c426
Liufang Yang <ylf1991scau@gmail.com>
2017-06-15 23:30:37 -0400

    introduct phamtom js to start the scrape job

---
+++ ServicesStation/Jobs/ScraperJob.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Quartz;
+using OpenQA.Selenium;
+using OpenQA.Selenium.PhantomJS;
+using System.Collections;
+
+
+namespace ServicesStation.Jobs
+{
+    class ScraperJob : IJob
+    {
+        public void Execute(IJobExecutionContext context)
+        {
+            var url = "https://trends.google.com/trends/";
+            ScrapeTrendingKeywordsAndUrl(url);
+        }
+
+        public static void ScrapeTrendingKeywordsAndUrl(string url) 
+        {
+            Hashtable TrendingKeywordsAndUrl = new Hashtable();
+
+            var options = new PhantomJSOptions();
+            options.AddAdditionalCapability("phantomjs.page.settings.userAgent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.116 Safari/537.36");
+
+            var driverService = PhantomJSDriverService.CreateDefaultService();
+
+            driverService.LocalToRemoteUrlAccess = true;
+            driverService.WebSecurity = false;
+            driverService.LoadImages = false;
+
+            var driver = new PhantomJSDriver(driverService, options);
+            try
+            {
+                driver.Navigate().GoToUrl(url);
+                var pagesource = driver.PageSource;
+                Console.Write(pagesource);
+            }
+            catch (Exception e)
+
+            {
+                e.ToString();
+                driver.Quit();
+
+            }
+            driver.Quit();
+
+        }
+
+    }
+}
--- ServicesStation/Program.cs
+++ ServicesStation/Program.cs
@@ -1,6 +1,6 @@
 ﻿using Topshelf;
 using ServicesStation.Jobs;
-
+using Quartz;
 
 namespace ServicesStation
 {
@@ -8,10 +8,16 @@ namespace ServicesStation
     {
         static void Main(string[] args)
         {
-            HostFactory.Run(c =>
-            {
-                Schedule.ScheduleJobsWithCrontab<HelloJob>(c, "0/1 * * * * ?");
-            });
+            var url = "https://trends.google.com/trends/";
+            ScraperJob.ScrapeTrendingKeywordsAndUrl(url);
+            //HostFactory.Run(c =>
+            //{            
+            //    c.SetServiceName("ServicesStation");
+            //    c.SetDisplayName("ServicesStation");
+            //    ScraperJob.ScrapeTrendingKeywordsAndUrl(url);
+            //    //Schedule.ScheduleJobsWithCrontab<HelloJob>(c, "0/1 * * * * ?");
+            //    //Schedule.ScheduleJobsWithCrontab<ScraperJob>(c, "
+            //});
         }
     }
 
